88ceca6808240364207881407f1111ff1869d73b
Benjamin <bb222bx@student.lnu.se>
2016-09-01 00:15:38 +0200

    Removed old code.

--- model/DBConn.php
+++ model/DBConn.php
@@ -24,30 +24,4 @@ class DBConn{
         }
     }
 
-    /**
-     * Get public file list
-     * @return array of the table
-     */
-    public function getPublicFileList(){
-
-        $sql = "SELECT * FROM ".DB_TABELLFILE;
-        $query = $this->pdo->prepare($sql);
-        $query->execute();
-       return $results = $query->fetchAll();
-    }
-
-    /**
-     * Get private file list for the logged in user
-     * @return mixed
-     */
-    public function getPrivateFileList(){
-
-        $username1 =$_SESSION['user'];
-        $sql = "SELECT * FROM ". DB_TABELLFILEPRIVATE ." WHERE username = :username";
-        $query = $this->pdo->prepare($sql);
-        $query->bindParam(':username',$username1);
-        $query->execute();
-        return $results = $query->fetchAll();
-    }
-
 }
